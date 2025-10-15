55d523a67cdd70a0d4b31a997cd6b2bada9e8a1c
К <dimanjex@gmail.com>
2025-10-15 10:31:19 +0700

    feat: menu

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+void displayMenu() {
+	cout << "\n=== КАЛЬКУЛЯТОР ===" << endl;
+	cout << "1. Сложение (+)" << endl;
+	cout << "2. Вычитание (-)" << endl;
+	cout << "3. Умножение (*)" << endl;
+	cout << "4. Деление (/)" << endl;
+	cout << "5. Выход" << endl;
+	cout << "Выберите операцию: ";
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int choice;
+
+	while (true) {
+		displayMenu();
+		cin >> choice;
+
+		if (std::cin.fail()) {
+			std::cout << "Ошибка! Введите только числа.\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+
+		if (choice == 7) {
+			cout << "До свидания!" << endl;
+			break;
+		}
+	}
+}
